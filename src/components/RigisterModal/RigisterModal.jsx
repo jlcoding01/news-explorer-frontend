241fe7cc90c45547db79ea24247dbfc5e31eaa15
Jerry Liu <ljliu89@gmail.com>
2025-01-20 21:28:56 -0800

    Fixed tag name

--- src/components/RigisterModal/RigisterModal.jsx
+++ src/components/RigisterModal/RigisterModal.jsx
@@ -30,62 +30,56 @@ function RigisterModal({
       isBtnDisabled={!isValid}
       onSubmit={handleSubmit}
     >
-      <>
-        <div className="form__element">
-          <label htmlFor="email-register" className="form__element_label">
-            Email
-          </label>
-          <input
-            type="email"
-            className="form__element_input"
-            placeholder="Enter email"
-            name="email"
-            id="email-register"
-            value={values["email"] || ""}
-            onChange={handleValueChange}
-            required
-          />
-          <span className="form__element_error-msg">
-            {errors["email"] || ""}
-          </span>
-        </div>
-        <div className="form__element">
-          <label htmlFor="password-register" className="form__element_label">
-            Password
-          </label>
-          <input
-            type="text"
-            className="form__element_input"
-            placeholder="Enter password"
-            name="password"
-            id="password-register"
-            value={values["password"] || ""}
-            onChange={handleValueChange}
-            required
-          />
-          <span className="form__element_error-msg">
-            {errors["password"] || ""}
-          </span>
-        </div>
-        <div className="form__element">
-          <label htmlFor="username-register" className="form__element_label">
-            Username
-          </label>
-          <input
-            type="text"
-            className="form__element_input"
-            placeholder="Enter your username"
-            name="name"
-            id="username-register"
-            value={values["name"] || ""}
-            onChange={handleValueChange}
-            required
-          />
-          <span className="form__element_error-msg">
-            {errors["name"] || ""}
-          </span>
-        </div>
-      </>
+      <div className="form__element">
+        <label htmlFor="email-register" className="form__element_label">
+          Email
+        </label>
+        <input
+          type="email"
+          className="form__element_input"
+          placeholder="Enter email"
+          name="email"
+          id="email-register"
+          value={values["email"] || ""}
+          onChange={handleValueChange}
+          required
+        />
+        <span className="form__element_error-msg">{errors["email"] || ""}</span>
+      </div>
+      <div className="form__element">
+        <label htmlFor="password-register" className="form__element_label">
+          Password
+        </label>
+        <input
+          type="text"
+          className="form__element_input"
+          placeholder="Enter password"
+          name="password"
+          id="password-register"
+          value={values["password"] || ""}
+          onChange={handleValueChange}
+          required
+        />
+        <span className="form__element_error-msg">
+          {errors["password"] || ""}
+        </span>
+      </div>
+      <div className="form__element">
+        <label htmlFor="username-register" className="form__element_label">
+          Username
+        </label>
+        <input
+          type="text"
+          className="form__element_input"
+          placeholder="Enter your username"
+          name="name"
+          id="username-register"
+          value={values["name"] || ""}
+          onChange={handleValueChange}
+          required
+        />
+        <span className="form__element_error-msg">{errors["name"] || ""}</span>
+      </div>
     </ModalWithForm>
   );
 }
